dedfad8f0a379be2bdb0d05c6bd7b98ee7c8c8c4
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-14 13:34:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 ﻿
+#include <list>
+
 #include <regex>
 #include <string>
 #include <string_view>
@@ -14,7 +16,7 @@ enum ErrorCodeC : int {
 #include <filesystem>
 namespace the {
     namespace filesystem = std::filesystem;
-    const auto getStreamFileName( const filesystem::path & arg ){
+    const auto getStreamFileName(const filesystem::path & arg) {
         return arg;
     }
 }/**/
@@ -22,7 +24,7 @@ namespace the {
 #include <experimental/filesystem>
 namespace the {
     namespace filesystem = std::experimental::filesystem;
-    auto getStreamFileName( const filesystem::path & arg ){
+    auto getStreamFileName(const filesystem::path & arg) {
         return arg.string();
     }
 }/**/
@@ -34,13 +36,13 @@ namespace the {
 namespace the {
 
     inline bool deleteDir(const filesystem::path & arg) try {
-        return true;
+        return filesystem::remove_all(arg);
     } catch (...) {
         return false;
     }
 
     inline bool deleteFile(const filesystem::path & arg) try {
-        return true;
+        return filesystem::remove(arg);
     } catch (...) {
         return false;
     }
@@ -67,21 +69,21 @@ namespace the {
 
                     auto varReallyTo = filesystem::canonical(varTo);
                     if (varReallyTo.filename() != varTo.filename()) {
-                        deleteFile(varReallyTo);
+                        filesystem::rename(varReallyTo, varTo);
                     }
 
                 }
 
             }
-                       
+
             std::ifstream varRead{ getStreamFileName(varFrom) , std::ios::binary };
             std::ofstream varWrite{ getStreamFileName(varTo)  , std::ios::binary };
 
-            if( false == varRead.is_open() ){
+            if (false == varRead.is_open()) {
                 return false;
             }
 
-            if( false == varWrite.is_open() ){
+            if (false == varWrite.is_open()) {
                 return false;
             }
 
@@ -92,10 +94,75 @@ namespace the {
         return false;
     }
 
-    inline bool copyADir(
-        const filesystem::path & varFrom,
-            const filesystem::path & varTo){
+    inline std::list< std::pair<filesystem::path, filesystem::path> >  _copyADir(
+        const filesystem::path & varFrom, const filesystem::path & varTo) {
 
+            {
+                /*创建根路径*/
+                auto varDir = varTo.parent_path();
+                filesystem::create_directories(varDir);
+            }
+
+            if (filesystem::exists(varTo)) {
+
+                if (!filesystem::is_directory(varTo)) {
+                    if (false == deleteFile(varTo)) {
+                        return {};
+                    }
+                }
+
+                auto varReallyTo = filesystem::canonical(varTo);
+                if (varReallyTo.filename() != varTo.filename()) {
+                    filesystem::rename(varReallyTo, varTo);
+                }
+
+            }
+
+            filesystem::directory_iterator varPos{ varFrom };
+            filesystem::directory_iterator varEnd;
+            std::list< std::pair<filesystem::path, filesystem::path> > varAns;
+
+            for (; varPos != varEnd; ++varPos) {
+
+                if (varPos->is_directory()) {
+
+                    varAns.emplace_back(varPos->path(), varTo / varPos->path().filename());
+
+                } else {
+
+                    if (false == copyAFile(varPos->path(), varTo / varPos->path().filename())) {
+                        return {};
+                    }
+
+                }
+
+            }
+
+            return std::move(varAns);
+
+    }
+
+    inline bool copyADir(
+        const filesystem::path & varFrom, const filesystem::path & varTo) try {
+
+
+        std::list< std::pair<filesystem::path, filesystem::path> > varDuty;
+        varDuty.emplace_back(varFrom, varTo);
+
+        while (!varDuty.empty()) {
+
+            auto varItem = varDuty.front();
+            varDuty.pop_front();
+
+            varDuty.splice(varDuty.begin(),
+                _copyADir(varItem.first, varItem.second));
+
+        }
+
+        return true;
+
+    } catch (...) {
+        return false;
     }
 
 }/*namespace the*/
@@ -111,11 +178,13 @@ int main(int argc, char ** argv) {
     the::filesystem::path varTo(argv[2]);
 
     if (the::filesystem::is_directory(varFrom)) {
-        if(false==the::copyADir(varFrom,varTo)){
+        if (false == the::copyADir(varFrom, varTo)) {
         }
+
     } else {
-        if(false==the::copyAFile(varFrom, varTo)){
+        if (false == the::copyAFile(varFrom, varTo)) {
         }
+
     }
 
     return 0;
